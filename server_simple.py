1fb41a41e1bb0d2d78eb6ba3f648e2e808b2882b
Tal Hadad <TalHadad@users.noreply.github.com>
2022-01-05 23:09:07 +0200

    Fix typo

--- server_simple.py
+++ server_simple.py
@@ -9,7 +9,7 @@ ip = socket.gethostname()
 listen_s.bind(('',port))
 
 listen_s.listen(max_connection)
-print(f'server started at {ip} on port {port})
+print(f'server started at {ip} on port {port}')
 
 (client_s, addresss) = listen_s.accept()
 print('new connection made')
