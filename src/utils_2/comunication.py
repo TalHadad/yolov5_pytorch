e4c7362216cb962c766c29c5c81a9b743de4c6ce
Tal Hadad <TalHadad@users.noreply.github.com>
2022-02-04 13:47:29 +0200

    Update comunication to msgpack (to send objects)

--- src/utils_2/comunication.py
+++ src/utils_2/comunication.py
@@ -1,20 +1,24 @@
 from typing import Any
 import zlib
 import pickle
+import msgpack
 
 def send(socket, msg: Any) -> None:
     """pickle an object, and zip the pickle before sending it"""
 
-    p = pickle.dumps(msg, protocol=-1)
-    z = p#zlib.compress(p)
+    #p = pickle.dumps(msg, protocol=-1)
+    #z = p#zlib.compress(p)
+    #z = zlib.compress(msg)
+    z = msgpack.packb(msg, use_bin_type=True)
     socket.send(z, flags=0)
 
 
 def receive(socket) -> Any:
     """inverse of send_zipped_pickle"""
     z = socket.recv(copy=False, flags=0)
-    p = z#zlib.decompress(z)
-    msg = pickle.loads(p)
+    #p = z#zlib.decompress(z)
+    #msg = pickle.loads(p)
+    msg = msgpack.unpackb(z, use_list=False, raw=False)
     return msg
 
 
