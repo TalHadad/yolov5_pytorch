c570f8ee9cac649652639db27d560a28df706fad
Tal Hadad <TalHadad@users.noreply.github.com>
2022-02-06 11:24:48 +0200

    Refactor test job handler.

---
+++ src/test_job_handler.py
@@ -0,0 +1,21 @@
+# test_job_handler.py
+import logging
+import time
+
+from client.controller import JobHandler
+from client.controller_raspi import ControllerRPi
+from utils_2.config_parser import ConfigReader
+
+def test_job_handler():
+    conf = ConfigReader().get_params()
+    controller_raspi = ControllerRPi(conf=conf)
+    action = 1
+    job_handler = JobHandler(controller_raspi, action)
+    logging.info(f'controller doning action {iter}: {action}')
+    job_handler = JobHandler(action)
+    job_handler.start()
+    time.sleep(5)
+    job_handler.join()
+
+if __name__ == "__main__":
+    test_job_handler()
